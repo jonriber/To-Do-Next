0a131ca5a6b112f4db5e3bb8b3f09db2fc89eae6
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-06-18 21:03:13 +0100

    first query of todos inside main page

---
+++ to-do-app/src/app/new/page.tsx
@@ -0,0 +1,9 @@
+
+
+const New = () => {
+    return <>
+        <h1>new</h1>
+    </>
+}
+
+export default New
--- to-do-app/src/app/page.tsx
+++ to-do-app/src/app/page.tsx
@@ -1,7 +1,20 @@
+import { prisma } from '@/db'
 import Image from 'next/image'
+import Link from 'next/link'
 
-export default function Home() {
-  return (
-    <h1></h1>
-  )
+export default async function Home() {
+  const todos = await prisma.todo.findMany()
+  return <>
+    <header className='flex justify-between items-center mb-4'>
+      <h1 className='text-2xl'>To-Dos</h1>
+      <Link className='border border-slate-300 text-slate-300 px-2 rounded 
+        hover:bg-slate-700 focus-within:bg-slate-700 outline-none
+      ' href={"/new"}>New</Link>
+    </header>  
+    <ul className='pl-4'>
+      {todos.map(todo => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  </>
 }
