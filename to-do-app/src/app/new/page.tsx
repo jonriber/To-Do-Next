9b71a946142d0b526ec960e14cbb479e981b7662
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-06-18 21:46:50 +0100

    styling new todo page

--- to-do-app/src/app/new/page.tsx
+++ to-do-app/src/app/new/page.tsx
@@ -1,8 +1,18 @@
-
+import { createTodo } from '@/functions/dbFunctions'
+import Link from 'next/link'
 
 const New = () => {
     return <>
-        <h1>new</h1>
+        <header className='flex justify-between items-center mb-4'>
+            <h1 className='text-2xl'>New Task</h1>
+        </header>
+        <form action={createTodo} className="flex gap-2 flex-col">
+            <input type="text" name="title" className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"/>
+            <div className='flex gap-1 justify-end'>
+                <Link href="..">Cancel</Link>
+                <button type='submit'>Create</button>
+            </div>
+        </form>
     </>
 }
 
--- to-do-app/src/functions/dbFunctions.ts
+++ to-do-app/src/functions/dbFunctions.ts
@@ -1,5 +1,15 @@
 import { prisma } from "@/db"
-
+import { redirect } from "next/navigation"
 export const getTodos =async () => {
     return prisma.todo.findMany()
+}
+
+export const createTodo =async (data:FormData) => {
+    "use server"
+    const title = data.get("title")?.valueOf()
+
+    if( typeof title !== "string" || title.length == 0) throw new Error("Invalid Title.") 
+
+    await prisma.todo.create({ data: { title, complete : false}})
+    redirect("/")
 }
